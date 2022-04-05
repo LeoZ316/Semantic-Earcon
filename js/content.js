fa33ccc2170c58502cce908d09f827fcfcd52762
LeoZ316 <95946291+LeoZ316@users.noreply.github.com>
2022-04-05 13:05:36 -0500

    Added Dropdown menu functionality

--- js/content.js
+++ js/content.js
@@ -1,13 +1,12 @@
-setInterval(() =>
-{
-    let match = location.href.match(/https:\/\/github\.com(?:\/[^\/]+)*\/commit\/[0-9a-f]{40}/i)
-    if (!match) return
-
-    let additions = document.querySelectorAll('.blob-code-addition:not([lang]')
-    additions.forEach(addition => addition.setAttribute('lang', 'en-US'))
-
-    let deletions = document.querySelectorAll('.blob-code-deletion:not([lang]')
-    deletions.forEach(deletion => deletion.setAttribute('lang', 'en-GB'))
-}, 1000)
-
-// Custom for lang tag and assign foucs + - and changed lines
+chrome.storage.local.get({langAdd: 'en-us', langDel: 'en-gb'}, ({langAdd, langDel}) => {
+    setInterval(() => {
+        let match = location.href.match(/https:\/\/github\.com(?:\/[^\/]+)*\/commit\/[0-9a-f]{40}/i)
+        if(!match) return
+        
+        let additions = document.querySelectorAll('.blob-code-addition:not([lang]')
+        additions.forEach(addition => addition.setAttribute('lang', langAdd))
+        
+        let deletions = document.querySelectorAll('.blob-code-deletion:not([lang]')
+        deletions.forEach(deletion => deletion.setAttribute('lang', langDel))
+    }, 1000)    
+})
---
+++ js/popup.js
@@ -0,0 +1,11 @@
+window.onload = async () => {
+    let {langAdd, langDel} = await chrome.storage.local.get({langAdd: 'en-us', langDel: 'en-gb'})
+    let langAddSelect = document.getElementById('language-add')
+    let langDelSelect = document.getElementById('language-del')
+
+    langAddSelect.value = langAdd
+    langAddSelect.onchange = e => chrome.storage.local.set({langAdd: langAddSelect.value})
+    
+    langDelSelect.value = langDel
+    langDelSelect.onchange = e => chrome.storage.local.set({langDel: langDelSelect.value})
+}
