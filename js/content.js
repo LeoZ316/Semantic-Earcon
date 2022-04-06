5294a5b60a2bf66d3d718501ca58ef2484d4045a
LeoZ316 <95946291+LeoZ316@users.noreply.github.com>
2022-04-06 10:15:02 -0500

    lang tag update

--- js/content.js
+++ js/content.js
@@ -1,12 +1,14 @@
-chrome.storage.local.get({langAdd: 'en-us', langDel: 'en-gb'}, ({langAdd, langDel}) => {
-    setInterval(() => {
+chrome.storage.local.get({ langAdd: 'en-US', langDel: 'en-GB' }, ({ langAdd, langDel }) =>
+{
+    setInterval(() =>
+    {
         let match = location.href.match(/https:\/\/github\.com(?:\/[^\/]+)*\/commit\/[0-9a-f]{40}/i)
-        if(!match) return
-        
+        if (!match) return
+
         let additions = document.querySelectorAll('.blob-code-addition:not([lang]')
         additions.forEach(addition => addition.setAttribute('lang', langAdd))
-        
+
         let deletions = document.querySelectorAll('.blob-code-deletion:not([lang]')
         deletions.forEach(deletion => deletion.setAttribute('lang', langDel))
-    }, 1000)    
+    }, 1000)
 })
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,12 @@
-window.onload = async () => {
-    let {langAdd, langDel} = await chrome.storage.local.get({langAdd: 'en-us', langDel: 'en-gb'})
+window.onload = async () =>
+{
+    let { langAdd, langDel } = await chrome.storage.local.get({ langAdd: 'en-US', langDel: 'en-GB' })
     let langAddSelect = document.getElementById('language-add')
     let langDelSelect = document.getElementById('language-del')
 
     langAddSelect.value = langAdd
-    langAddSelect.onchange = e => chrome.storage.local.set({langAdd: langAddSelect.value})
-    
+    langAddSelect.onchange = e => chrome.storage.local.set({ langAdd: langAddSelect.value })
+
     langDelSelect.value = langDel
-    langDelSelect.onchange = e => chrome.storage.local.set({langDel: langDelSelect.value})
+    langDelSelect.onchange = e => chrome.storage.local.set({ langDel: langDelSelect.value })
 }
