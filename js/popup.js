4992e47d6eb458854cd499cef17c28cfe4f0fc16
LeoZ316 <95946291+LeoZ316@users.noreply.github.com>
2022-04-13 14:08:13 -0500

    Fixed vocalizer

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,12 @@
-window.onload = async () => {
-    let {langAdd, langDel} = await chrome.storage.local.get({langAdd: 'en-us', langDel: 'en-gb'})
+window.onload = async () =>
+{
+    let { langAdd, langDel } = await chrome.storage.local.get({ langAdd: 'en-GB', langDel: 'en-AU' })
     let langAddSelect = document.getElementById('language-add')
     let langDelSelect = document.getElementById('language-del')
 
     langAddSelect.value = langAdd
-    langAddSelect.onchange = e => chrome.storage.local.set({langAdd: langAddSelect.value})
-    
+    langAddSelect.onchange = e => chrome.storage.local.set({ langAdd: langAddSelect.value })
+
     langDelSelect.value = langDel
-    langDelSelect.onchange = e => chrome.storage.local.set({langDel: langDelSelect.value})
+    langDelSelect.onchange = e => chrome.storage.local.set({ langDel: langDelSelect.value })
 }
